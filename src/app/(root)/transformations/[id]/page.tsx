a4aca5c410c1102ed6c20abf1d793da3566085d8
Jhenbert <jvv202726@gmail.com>
2024-12-13 01:24:54 +0800

    fix: transformations[id]/

--- src/app/(root)/transformations/[id]/page.tsx
+++ src/app/(root)/transformations/[id]/page.tsx
@@ -9,7 +9,11 @@ import { getImageById } from "@/lib/actions/image.actions";
 import { getImageSize } from "@/lib/utils";
 import { DeleteConfirmation } from "@/components/shared/DeleteConfirmation";
 
-const ImageDetails = async ({ params: { id } }: SearchParamProps) => {
+const ImageDetails = async ({
+  params: { id },
+}: {
+  params: { [key: string]: string };
+}) => {
   const { userId } = await auth();
 
   const image = await getImageById(id);
